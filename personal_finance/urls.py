f9ec0150141e477e38cb6c5390674de3aa76c73a
pranavv-dev-python <pranavvijay2000@gmail.com>
2026-03-25 13:08:36 +0530

    default response added

--- personal_finance/urls.py
+++ personal_finance/urls.py
@@ -14,13 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.http import JsonResponse
 from django.contrib import admin
 from django.urls import path , include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+def home(request):
+    return JsonResponse({"message": "API is working"})
+
+
 urlpatterns = [
+    path('', home),
 
     path('admin/', admin.site.urls),
+    
     path('api/v1/' , include('accounts.urls')) ,
     path('api/v1/' , include('expenses.urls')) ,
     path('api/v1/' , include('budgets.urls')) ,
